383a4098d0dc7a896465168359e5706e9d69f005
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-05-12 18:18:12 +0200

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -50,7 +50,32 @@ app.delete('/todos/:id',function(req,res){
 	}else{
 		res.status(404).send();
 	}
-	
+});
+//put /todos/:id
+app.put('/todos/:id',(req,res) => {
+		var todoId = parseInt(req.params.id,10);
+		var body = _.pick(req.body,"description","completed");
+		var validAttributes = {};
+		var matchedTodo = _.findWhere(todos,{id:todoId});
+
+		if(!matchedTodo){
+			return res.status(404).send();
+		}
+
+		if(body.hasOwnProperty('completed')&& _.isBoolean(body.completed)){
+			validAttributes.completed = body.completed;
+		} else if(body.hasOwnProperty('completed')){
+			return res.status(400).send();
+		}
+		if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+			validAttributes.description = body.description;
+		} else if(body.hasOwnProperty('description')){
+			return res.status(400).send();
+		}
+		
+		_.extend(matchedTodo,validAttributes);
+		res.json(matchedTodo);
+
 
 });
 app.listen(PORT,function(){
