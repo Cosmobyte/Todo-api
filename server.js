48a1d03ab842f7f9ba08d0e31447e21fbfe045ff
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-05-28 01:13:23 +0200

    Add login route and authenticate class methods

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcryptjs');
 
 var todos = [];
 var todoNextId = 1;
@@ -121,7 +122,19 @@ app.put('/todos/:id',(req,res) => {
 		});
 
 });
-db.sequelize.sync().then(function(){
+
+//POST /user/login
+app.post('/users/login', function(req,res){
+	var body = _.pick(req.body, 'email','password');
+
+	db.user.authenticate(body).then(function(user){
+		res.json(user.toPublicJSON());
+	},function(){
+		res.status(401).send();
+	});
+	
+});
+db.sequelize.sync({force:true}).then(function(){
 	app.listen(PORT,function(){
 	console.log('Express listening on port' + PORT +'!');
 	});
